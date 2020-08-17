893f94288af46e39cee1788c2637ea14b59f0c8c
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-08-17 19:49:23 +0200

    proxmox-provisioning-server: add container model

--- proxmox-provisioning-server/main.rb
+++ proxmox-provisioning-server/main.rb
@@ -1,17 +1,38 @@
+# web
 require 'sinatra'
 require "sinatra/namespace"
 
+# tracing
 require 'opentracing'
 require 'jaeger/client'
 require 'rack/tracer'
 
+# proxmox
+
+# tracing init
 OpenTracing.global_tracer = Jaeger::Client.build(service_name: 'proxmox-provisioning-server')
 
 use Rack::Tracer
 
-# DB Setup - Hidden
+# Models
+class Container
+  def initialize(id, hostname, disk, ram)
+    @id         = id
+    @hostname   = hostname
+    @disk       = disk
+    @ram        = ram
+  end
 
-# Models - Hidden
+  def all(*)
+    data = {
+        id:"1",
+        hostname:"test1"
+    }
+    data
+  end
+end
+
+containers = Container.new("1", "test1", 260, "")
 
 # Endpoints
 get '/' do
@@ -29,6 +50,6 @@ namespace '/api' do
   end
 
   get '/containers' do
-    #containers.all.to_json
+    containers.all.to_json
   end
 end

81ba7d69fc27413fa83a4e7eeaae1cfb7205e337
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-08-17 19:21:03 +0200

    proxmox-provisioning-server: simple endpoints

--- proxmox-provisioning-server/main.rb
+++ proxmox-provisioning-server/main.rb
@@ -15,15 +15,20 @@ use Rack::Tracer
 
 # Endpoints
 get '/' do
-  '<html><body>proxmox-provisioning-server index page</body></html>'
+  "<html><body>proxmox-provisioning-server index page</body></html>\n"
 end
 
-namespace '/api ' do
+# API Endpoints
+namespace '/api' do
   before do
     content_type 'application/json'
   end
 
+  get do
+    "proxmox-provisioning-server api\n"
+  end
+
   get '/containers' do
-    containers.all.to_json
+    #containers.all.to_json
   end
 end

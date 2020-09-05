7c6e8f878b5b35cefb445d26d8efc780686ea886
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-09-05 20:06:04 +0200

    fix scripts for tf-owncloud handling

--- tf-owncloud/scripts/tf-owncloud-generate-tfvars.sh
+++ tf-owncloud/scripts/tf-owncloud-generate-tfvars.sh
@@ -6,7 +6,9 @@ echo "----------------------------------------"
 echo "generate ID, name and IP address..."
 echo "----------------------------------------"
 
-count=`sudo find /root/repos/h-platform-automation-cm-tf-owncloud -name '*.tfvars' ! -name "terraform.tfvars" -type f -print  |sort -h`
+wd="/root/h-platform-automation-core/tf-owncloud"
+
+count=`find ${wd} -name '*.tfvars' ! -name oc-xyz.tfvars ! -name "terraform.tfvars" -type f -print  |sort -h`
 
 if [ "$count" == "" ]; then
   echo "first file.."
@@ -15,7 +17,7 @@ if [ "$count" == "" ]; then
   vmip='10.210.65.101'
 else
   echo "file(s) found..";
-  last=`sudo find /root/repos/h-platform-automation-cm-tf-owncloud -name '*.tfvars' ! -name "terraform.tfvars" -type f -print 2>/dev/null |sort -h | tail -1 |grep -o '[0-9][0-9][0-9]'`
+  last=`find ${wd} -name '*.tfvars' ! -name oc-xyz.tfvars ! -name "terraform.tfvars" -type f -print 2>/dev/null |sort -h | tail -1 |grep -o '[0-9][0-9][0-9]'`
   echo "last id: ${last}";
   vmid=$((last + 1))
   vmname="oc-"${vmid}
@@ -25,15 +27,15 @@ fi;
 
 if [ ${vmid} -le 300 ]; then
   echo -e "ERROR: generating ID and IP addresss failed! \nExiting..\n";
-  echo -e "vmid=\nvmname=\nvmip=\n" > export_props.properties;
+  echo -e "vmid=\nvmname=\nvmip=\n" > ${wd}/export_props.properties;
   exit 11;
 else
   echo -e "\nINFO: generating artifacts.."
   # save vars into artifact
-  echo -e "vmid=${vmid}\nvmname=oc-${vmid}\nvmip=${vmip}\ndomain=${domain}\n" > export_props.properties || exit 1;
+  echo -e "vmid=${vmid}\nvmname=oc-${vmid}\nvmip=${vmip}\ndomain=${domain}\n" > ${wd}/export_props.properties || exit 1;
   # save vars into tf file
-  echo -e "vmname = \"${vmname}\"\nvmip = \"${vmip}\"\nvm_data_disk_size = \"${datastore_size}\"\n" | sudo tee /root/repos/h-platform-automation-cm-tf-owncloud/${vmname}.tfvars || exit 1;
+  echo -e "vmname = \"${vmname}\"\nvmip = \"${vmip}\"\nvm_data_disk_size = \"${datastore_size}\"\n" | tee ${wd}/${vmname}.tfvars || exit 1;
 fi;
 
 end=`date +%s`
-echo -e "\n time: "$((end-start))" seconds"
+echo -e "\ntime: "$((end-start))" seconds"
--- tf-owncloud/scripts/tf-owncloud-run-terraform.sh
+++ tf-owncloud/scripts/tf-owncloud-run-terraform.sh
@@ -4,12 +4,20 @@ echo "----------------------------------------"
 echo "create virtual machine and customize it..."
 echo "----------------------------------------"
 
-source export_props.properties;
+wd="/root/h-platform-automation-core/tf-owncloud"
+tfbin="/opt/terraform_0.13.2/terraform"
+
+source ${wd}/export_props.properties;
 
 echo "INFO: sourced artifacts.."
 echo "sourced vmid: ${vmid}";
 echo "sourced vmname: ${vmname}";
 echo "sourced vmip: ${vmip}";
+
+if [ "x${vmid}" == "x" ]; then
+  echo -e "ERROR: sourced ID missing! \nExiting..\n";
+  exit 12;
+fi;
 
 if [ ${vmid} -le 300 ]; then
   echo -e "ERROR: sourced ID and IP addresss are wrong! \nExiting..\n";
@@ -21,9 +29,9 @@ echo -e "\nINFO: running terraform plan & apply.."
 #sudo /usr/bin/rm /root/repos/h-platform-automation-cm-tf-owncloud/terraform.tfstate -f || exit 1;
 #sudo /usr/bin/rm /root/repos/h-platform-automation-cm-tf-owncloud/terraform.tfstate.backup -f || exit 1;
 
-sudo bash -c "cd /root/repos/h-platform-automation-cm-tf-owncloud && /root/scripts/terraform plan -no-color -input=false -var-file=\"${vmname}.tfvars\" -state=\"terraform.tfstate.${vmname}\"; exit \$?" || exit 1;
+bash -c "cd ${wd} && ${tfbin} plan -no-color -input=false -var-file=\"${vmname}.tfvars\" -state=\"terraform.tfstate.${vmname}\"; exit \$?" || exit 1;
 
-sudo bash -c "cd /root/repos/h-platform-automation-cm-tf-owncloud && /root/scripts/terraform apply -auto-approve -no-color -input=false -var-file=\"${vmname}.tfvars\" -state=\"terraform.tfstate.${vmname}\"; exit \$?" || exit 1;
+bash -c "cd ${wd} && ${tfbin} apply -auto-approve -no-color -input=false -var-file=\"${vmname}.tfvars\" -state=\"terraform.tfstate.${vmname}\"; exit \$?" || exit 1;
 
 #sudo /usr/bin/rm /root/repos/h-platform-automation-cm-tf-owncloud/terraform.tfstate -f || exit 1;
 #sudo /usr/bin/rm /root/repos/h-platform-automation-cm-tf-owncloud/terraform.tfstate.backup -f || exit 1;
@@ -34,4 +42,4 @@ sudo bash -c "cd /root/repos/h-platform-automation-cm-tf-owncloud && /root/scrip
 #sleep 300;
 
 end=`date +%s`
-echo -e "\n time: "$((end-start))" seconds"
+echo -e "\ntime: "$((end-start))" seconds"
